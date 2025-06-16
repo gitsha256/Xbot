66d4770e364e1cf2294d6adbeaefe11fd99d036c
gitsha256 <signshailesh@gmail.com>
2025-06-16 15:27:46 +0530

    Delete gunicorn.conf.py

--- gunicorn.conf.py
+++
@@ -1,7 +0,0 @@
-loglevel = "debug"
-accesslog = "-"
-errorlog = "-"
-access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
-timeout = 600
-workers = 1
-preload_app = True  # Preload app to catch initialization errors
